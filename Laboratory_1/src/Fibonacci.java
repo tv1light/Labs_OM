fc8ddb9faa8ad564e1998eb542c98aebf22f9c42
Vlad <vlodik.guterian@gmail.com>
2024-03-24 15:49:19 +0400

    Preparation

--- Laboratory_1/src/Fibonacci.java
+++ Laboratory_1/src/Fibonacci.java
@@ -1,3 +1,4 @@
+
 public class Fibonacci {
     public static void fibonacci(int n){
 
---
+++ Laboratory_1/src/Function.java
@@ -0,0 +1,8 @@
+//Task function: y = sh^2(2x)   x on [-1, 1.5]
+
+public class Function {
+
+    public static double function(double x) {
+        return Math.pow(Math.sinh(2*x), 2);
+    }
+}
