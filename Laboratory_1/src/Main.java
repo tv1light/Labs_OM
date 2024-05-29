ed7920ecfe7472a9ef2353aaff0f40667f297d43
Vlad <vlodik.guterian@gmail.com>
2024-05-29 12:14:02 +0400

    Исрпавил избыточность вычислений для золотого сечения и метода Фибоначчи

--- Laboratory_1/src/Main.java
+++ Laboratory_1/src/Main.java
@@ -13,9 +13,9 @@ public class Main {
 
     public static void dih(double a, double b, double e){
 
-        double xc = 0;
-        double x1 = 0;
-        double x2 = 0;
+        double xc;
+        double x1;
+        double x2;
         int k = 0;
         while (Math.abs(a - b) > e){
             xc = (a+b)/2;
@@ -37,22 +37,28 @@ public class Main {
         while ((b - a)/eps > Fibonacci.fibonacci(n+2)){
             n++;
         }
-        double e = ((b-a)/ Fibonacci.fibonacci(n));
         double x1 = a + (double) Fibonacci.fibonacci(n - 2) * (b-a) /Fibonacci.fibonacci(n);
         double x2 = a + (double) Fibonacci.fibonacci(n - 1) * (b-a) /Fibonacci.fibonacci(n);
         int iterations = n+2;
+        double y1 = Function.function(x1);
+        double y2 = Function.function(x2);
+
         //Основной этап
         while (n >= 2){
             n--;
-            if(Function.function(x1) > Function.function(x2)){
+            if(y1 > y2){
                 a = x1;
                 x1 = x2;
+                y1 = y2;
                 x2 = a + (double) Fibonacci.fibonacci(n - 1) * (b-a) /Fibonacci.fibonacci(n);
+                y2 = Function.function(x2);
             }
             else{
                 b = x2;
                 x2 = x1;
+                y2 = y1;
                 x1 = a + (double) Fibonacci.fibonacci(n - 2) * (b-a) /Fibonacci.fibonacci(n);
+                y1 = Function.function(x1);
             }
 
         }
@@ -63,20 +69,26 @@ public class Main {
      public static void GoldenSection(double a, double b, double eps)
      {
          double x1 =a+(b-a)*(3-sqrt(5))/2, x2 =a+(b-a)*(sqrt(5)-1)/2;
+         double y1 = Function.function(x1);
+         double y2 = Function.function(x2);
          int iterations = 2;
          while(b-a > eps)
          {
-             if(Function.function(x1)<=Function.function(x2))
+             if(y1<=y2)
              {
                  b = x2;
                  x2 = x1;
+                 y2 = y1;
                  x1 = a +(b-a)*(3-sqrt(5))/2;
+                 y1 = Function.function(x1);
              }
              else
              {
                  a = x1;
                  x1 = x2;
+                 y1 = y2;
                  x2 = a+(b-a)*(sqrt(5)-1)/2;
+                 y2 = Function.function(x2);
              }
              ++iterations;
          }
